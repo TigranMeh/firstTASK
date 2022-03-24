a52d98ca0f0ceb6e00db699727ff6d93d2d381f0
Tigran <tmehrabyan@gmail.com>
2022-03-24 20:29:21 +0400

    First Program

---
+++ src/com/company/b.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class b {
+    public static void main(String[] args) {
+  /*  int i=6;
+    switch (i){
+        case 1:
+        case 2:
+        case 12:
+            System.out.println("dzmer");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            System.out.println("garun");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            System.out.println("amar");
+        case 9:
+        case 10:
+        case 11:
+            System.out.println("ashun");
+            break;
+        default:
+            System.out.println("ayd tvov amis chka");
+    }*/
+        /*int n;
+        for (n=0;n<=10  ; ++n ) {
+            System.out.println(+n);
+        }*/
+
+
+    }
+}
+
+
